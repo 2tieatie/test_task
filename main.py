a70c025b8b594e2183395d1ac23fa3473104addb
2tieatie <103947853+2tieatie@users.noreply.github.com>
2023-07-23 14:53:23 +0300

    docstrings main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from contact_info_links_scrapper import LIProfilesScrapper as Lips
 from contact_info_scrapper import LIInfoScrapper as Liis
 
+#classes init
 lips = Lips()
 liis = Liis()
+#setting first page
 page: int = 0
 
 while True:
     page += 1
     lips.get_links(page)
     for link in lips.links:
+        #data output
         liis.get_info(link)
         print(f'\n----{liis.name}----')
         print(f'Location: {liis.location}')
