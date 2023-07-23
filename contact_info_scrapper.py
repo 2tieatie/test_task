0b6fc89ce864d66e65f5ec4f59eeb99abf05a80f
2tieatie <103947853+2tieatie@users.noreply.github.com>
2023-07-23 22:01:12 +0300

    Update contact_info_scrapper.py

--- contact_info_scrapper.py
+++ contact_info_scrapper.py
@@ -5,11 +5,13 @@ from selenium.common.exceptions import NoSuchElementException
 
 
 class LIInfoScrapper:
-    
+
     def __init__(self):
         options: webdriver.EdgeOptions = webdriver.EdgeOptions()
         for option in driver_options:
             options.add_argument(option)
+        for key, value in custom_headers.items():
+            options.add_argument(f"--header={key}:{value}")
         self.driver: webdriver.Edge = webdriver.Edge(options=options)
         self.DEBUG: bool = True
         self.info: str
@@ -17,20 +19,25 @@ class LIInfoScrapper:
         self.location: str
 
     def get_info(self, link: str):
-        #collecting data from html by xpath
+        # collecting data from html by xpath
         self.driver.get(link)
-        self.name = self.driver.find_element(By.XPATH, "//h1[@id='pv-contact-info']").text.strip()
 
         try:
-            self.location = self.driver.find_element(By.XPATH, '//span[@class="text-body-small inline t-black--light break-words"]').text.strip()
+            self.name = self.driver.find_element(By.XPATH, "//h1[@id='pv-contact-info']").text.strip()
+        except Exception as ex:
+            self.name = None
+
+        try:
+            self.location = self.driver.find_element(By.XPATH,
+                                                     '//span[@class="text-body-small inline t-black--light break-words"]').text.strip()
         except NoSuchElementException:
             self.location = None
 
         try:
-            info_raw = self.driver.find_element(By.XPATH, "//div[@class='pv-profile-section__section-info section-info']")
+            info_raw = self.driver.find_element(By.XPATH,
+                                                "//div[@class='pv-profile-section__section-info section-info']")
             self.info = info_raw.text.strip().replace(f'{self.name.split()[0]}’s Profile\n', '')
         except NoSuchElementException:
             self.info = None
-
 
 
